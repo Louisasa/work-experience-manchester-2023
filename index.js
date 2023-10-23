f00b591474462d1a9158cd1a0eda7a06e19fc53b
Joe Anderton <joe.anderton07@gmail.com>
2023-10-23 14:37:18 +0100

    Displays User's Turn
    Uses a function with a true/false variable. States that it's player one's turn in the centre of the screen till the variable is false, then it does the same with player two.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var version = "Oware";
-
 document.getElementsByClassName("mancala-game-type")[0].innerHTML = version;
+var turnPlayerOne =true;
+
+function PlayerTurn() {
+if (turnPlayerOne) {
+    document.getElementsByClassName("main-game")[0].innerHTML ="It's Player 1's Turn!"
+    turnPlayerOne =false;
+}
+else {
+    document.getElementsByClassName("main-game")[0].innerHTML ="It's Player 2's Turn!"
+    turnPlayerOne =true;
+}
+}
+PlayerTurn();
