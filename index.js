520e0351e13d0efd42b6c9fd506182537811fa8d
Hamid Mirza <hamid.bmirza@gmail.com>
2023-10-23 15:52:51 +0100

    created making a move and update seeds

--- index.js
+++ index.js
@@ -1,87 +1,81 @@
 var version = "Oware";
-document.getElementsByClassName("mancala-game-type")[0].innerHTML = version;
-
 var HasGameStarted = false; 
 var GameStateButtonText = 'Start Game';
+var initialHouseNumber = "4";
+var initialScoreNumber = "0";
+var turnPlayerOne = true;
+
+document.getElementsByClassName("mancala-game-type")[0].innerHTML = version;
 document.getElementsByClassName("game-state")[0].innerHTML = GameStateButtonText;
 
 function startGame() {
     if (HasGameStarted){
         HasGameStarted = false;
-    } else {
-        HasGameStarted = true;
-    }
-} 
-
-
-function updateGameStateButtonText() {
-    if (HasGameStarted){
         GameStateButtonText = "Restart Game";
     } else {
+        HasGameStarted = true;
         GameStateButtonText = "Start Game";
     }
     document.getElementsByClassName("game-state")[0].innerHTML = GameStateButtonText;
+    setupGame();
+    PlayerTurn();
+} 
+
+function setupGame() {
+    document.getElementsByClassName("player-1-house-1 seeds")[0].innerHTML = initialHouseNumber;
+    document.getElementsByClassName("player-1-house-2 seeds")[0].innerHTML = initialHouseNumber;
+    document.getElementsByClassName("player-1-house-3 seeds")[0].innerHTML = initialHouseNumber;
+    document.getElementsByClassName("player-1-house-4 seeds")[0].innerHTML = initialHouseNumber;
+    document.getElementsByClassName("player-1-house-5 seeds")[0].innerHTML = initialHouseNumber;
+    document.getElementsByClassName("player-1-house-6 seeds")[0].innerHTML = initialHouseNumber;
+
+    document.getElementsByClassName("player-2-house-1 seeds")[0].innerHTML = initialHouseNumber;
+    document.getElementsByClassName("player-2-house-2 seeds")[0].innerHTML = initialHouseNumber;
+    document.getElementsByClassName("player-2-house-3 seeds")[0].innerHTML = initialHouseNumber;
+    document.getElementsByClassName("player-2-house-4 seeds")[0].innerHTML = initialHouseNumber;
+    document.getElementsByClassName("player-2-house-5 seeds")[0].innerHTML = initialHouseNumber;
+    document.getElementsByClassName("player-2-house-6 seeds")[0].innerHTML = initialHouseNumber;
+    
+    document.getElementsByClassName("player-1 score")[0].innerHTML = initialScoreNumber;
+    document.getElementsByClassName("player-2 score")[0].innerHTML = initialScoreNumber;
 }
-
-function change_game_state(){
-    startGame();
-    updateGameStateButtonText();
-}
-
-document.getElementsByClassName("mancala-game-type")[0].innerHTML = version;
-
-var turnPlayerOne =true;
-
-var p1h6 = "4";
-document.getElementsByClassName("player-1-house-6 seeds")[0].innerHTML = p1h6;
-
-var p2h1 = "4";
-document.getElementsByClassName("player-2-house-1 seeds")[0].innerHTML = p2h1;
-
-var p1h5 = "4";
-document.getElementsByClassName("player-1-house-5 seeds")[0].innerHTML = p1h5;
-
-var p2h2 = "4";
-document.getElementsByClassName("player-2-house-2 seeds")[0].innerHTML = p2h2;
-
-var p1h4 = "4";
-document.getElementsByClassName("player-1-house-4 seeds")[0].innerHTML = p1h4;
-
-var p2h3 = "4";
-document.getElementsByClassName("player-2-house-3 seeds")[0].innerHTML = p2h3;
-
-var p1h3 = "4";
-document.getElementsByClassName("player-1-house-3 seeds")[0].innerHTML = p1h3;
-
-var p2h4 = "4";
-document.getElementsByClassName("player-2-house-4 seeds")[0].innerHTML = p2h4;
-
-var p1h2 = "4";
-document.getElementsByClassName("player-1-house-2 seeds")[0].innerHTML = p1h2;
-
-var p2h5 = "4";
-document.getElementsByClassName("player-2-house-5 seeds")[0].innerHTML = p2h5;
-
-var p1h1 = "4";
-document.getElementsByClassName("player-1-house-1 seeds")[0].innerHTML = p1h1;
-
-var p2h6 = "4";
-document.getElementsByClassName("player-2-house-6 seeds")[0].innerHTML = p2h6;
-
-var p1score = "0";
-document.getElementsByClassName("player-1 score")[0].innerHTML = p1score;
-
-var p2score = "0";
-document.getElementsByClassName("player-2 score")[0].innerHTML = p2score;
 
 function PlayerTurn() {
-if (turnPlayerOne) {
-    document.getElementsByClassName("main-game")[0].innerHTML ="It's Player 1's Turn!"
-    turnPlayerOne =false;
+    if (turnPlayerOne) {
+        document.getElementsByClassName("main-game")[0].innerHTML ="It's Player 1's Turn!"
+        turnPlayerOne =false;
+    }
+    else {
+        document.getElementsByClassName("main-game")[0].innerHTML ="It's Player 2's Turn!"
+        turnPlayerOne =true;
+    }
 }
-else {
-    document.getElementsByClassName("main-game")[0].innerHTML ="It's Player 2's Turn!"
-    turnPlayerOne =true;
+
+function makeAMove(playerNumber, houseNumber) {
+    // Figure out how many seeds are in the house
+    var numberOfSeeds = document.getElementsByClassName(`player-${playerNumber}-house-${houseNumber} seeds`)[0].innerHTML;
+
+    // Empty this house
+    var amountleft = 0;
+    document.getElementsByClassName(`player-${playerNumber}-house-${houseNumber} seeds`)[0].innerHTML = amountleft
+
+    var houseIndex = houseNumber+1;
+    var playerNumberToUpdate = playerNumber;
+
+    // Add seeds clockwise
+    for(var index = 0; index < numberOfSeeds; index++ ) {
+        if (houseIndex > 6) {
+            houseIndex = 1;
+            if (playerNumberToUpdate===1){
+                playerNumberToUpdate=2
+
+            } else{
+                playerNumberToUpdate =1
+            } 
+        }
+        var seedsMoved=document.getElementsByClassName(`player-${playerNumberToUpdate}-house-${houseIndex} seeds`)[0].innerHTML;
+        document.getElementsByClassName(`player-${playerNumberToUpdate}-house-${houseIndex} seeds`)[0].innerHTML = parseInt(seedsMoved)+1;
+        houseIndex++;
+    }
+
 }
-}
-PlayerTurn();
